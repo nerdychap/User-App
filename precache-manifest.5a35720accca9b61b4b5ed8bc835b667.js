3632ca3df7213bac63e32b9aefa3207228909559
siyabonga <siyabonga.hlengwa@mirumagency.com>
2020-09-08 16:31:56 +0200

    Updates

---
+++ precache-manifest.5a35720accca9b61b4b5ed8bc835b667.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "035e22a6b451240fac5d75548c9a1118",
+    "url": "/User-App/index.html"
+  },
+  {
+    "revision": "91c5209e5e25a439a46d",
+    "url": "/User-App/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "ef6acb4d29b1c0c12549",
+    "url": "/User-App/static/css/main.6673b95d.chunk.css"
+  },
+  {
+    "revision": "91c5209e5e25a439a46d",
+    "url": "/User-App/static/js/2.68dd60d9.chunk.js"
+  },
+  {
+    "revision": "d3031346469a8a2ef527f1ad705ea3b7",
+    "url": "/User-App/static/js/2.68dd60d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d2472746eb30dc2d31c",
+    "url": "/User-App/static/js/3.34c10b43.chunk.js"
+  },
+  {
+    "revision": "8e7fa176b006150306288bd092a696c0",
+    "url": "/User-App/static/js/3.34c10b43.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79e72446488c8c0a789e",
+    "url": "/User-App/static/js/4.4ac65de8.chunk.js"
+  },
+  {
+    "revision": "ef6acb4d29b1c0c12549",
+    "url": "/User-App/static/js/main.7ee23ae8.chunk.js"
+  },
+  {
+    "revision": "792f0a130bf5437ca5c5",
+    "url": "/User-App/static/js/runtime-main.d24dd2b2.js"
+  }
+]);
